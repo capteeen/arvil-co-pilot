d236d259aab8b2f505e030405de9e329d1bea5fa
“capteen” <lmnjfar@gmail.com>
2025-03-24 21:12:51 +0100

    chore: bump version to 0.3.1

--- arvil/src/cli.js
+++ arvil/src/cli.js
@@ -13,7 +13,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // Version
-const VERSION = '0.3.0';
+const VERSION = '0.3.1';
 
 // Add error handling to prevent crashes
 process.on('uncaughtException', (error) => {
